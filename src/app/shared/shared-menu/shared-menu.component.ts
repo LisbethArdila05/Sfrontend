112338e165aabff7773ef67e4bae9ca73d69dd20
LisbethArdila05 <ardilabernal5@gmail.com>
2024-05-04 13:47:03 -0500

    1024responsive

--- src/app/shared/shared-menu/shared-menu.component.ts
+++ src/app/shared/shared-menu/shared-menu.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-shared-menu',
   templateUrl: './shared-menu.component.html',
   styleUrls: ['./shared-menu.component.scss'],
 })
 export class SharedMenuComponent  implements OnInit {
+  
+  menu: boolean = true
+  
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(this.innerWidth() <= 1024){
+      this.menu = false;
+    }
+  }
+
+  verMenu(){
+    this.menu = !this.menu 
+  }
+  innerWidth(): number{
+    return window.innerWidth;
+  }
 
 }
